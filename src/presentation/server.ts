a9fd7ee17e955768abc4279fae7aecaa1487f2a0
Abdiel Alvarado <saam.0926@gmail.com>
2026-05-21 16:38:27 -0600

    Simple CRUD created

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'node:path';
 
 interface Options {
   port: number;
+  routes: Router;
   publicPath?: string;
 }
 
@@ -11,20 +12,28 @@ export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, publicPath = 'public' } = options;
+    const { port, publicPath = 'public', routes } = options;
     this.port = port;
     this.publicPath = publicPath;
+    this.routes = routes
   }
 
   async start() {
 
     //* Middlewares
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
 
     //* Public Folder
     this.app.use(express.static(this.publicPath));
 
+    //* Routes
+    this.app.use(this.routes);
+
+    //*
     this.app.get('/*splat', (req, res) => {
       const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
       res.sendFile(indexPath);
