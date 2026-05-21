2738ca548ee71ab6c2b6d45d8812cb297c5aeda2
Abdiel Alvarado <saam.0926@gmail.com>
2026-05-20 18:31:42 -0600

    First commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import path from 'node:path';
+
+interface Options {
+  port: number;
+  publicPath?: string;
+}
+
+export class Server {
+
+  private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+
+  constructor(options: Options) {
+    const { port, publicPath = 'public' } = options;
+    this.port = port;
+    this.publicPath = publicPath;
+  }
+
+  async start() {
+
+    //* Middlewares
+
+    //* Public Folder
+    this.app.use(express.static(this.publicPath));
+
+    this.app.get('/*splat', (req, res) => {
+      const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
+      res.sendFile(indexPath);
+    });
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port: ${3000}`);
+    });
+  }
+}
